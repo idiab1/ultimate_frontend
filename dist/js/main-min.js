dbeffa70a50f94ac4a6f8352857a2605c6d3030e
islamdiab-stack <islamdiab967@gmail.com>
2020-07-08 02:40:58 +0200

    style for sidebar

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".sidebar, .content-area").toggleClass("no-sidebar")})});
+$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".sidebar, .content-area").toggleClass("no-sidebar")}),$(".toggle-submenu").on("click",function(){$(this).find(".fa-angle-right").toggleClass("down"),$(this).next(".child-links").slideToggle()})});
--- stage/js/main.js
+++ stage/js/main.js
@@ -1,8 +1,11 @@
 $(function(){
   'use strict';
   $('.toggle-sidebar').on("click", function(){
-    
+
     $('.sidebar, .content-area').toggleClass('no-sidebar');
-    
+  });
+  $('.toggle-submenu').on('click', function(){
+    $(this).find('.fa-angle-right').toggleClass('down');
+    $(this).next('.child-links').slideToggle();
   })
 })
